2e02ce8d5a395613ad8442b6eb32bf9c82df05a5
chrisdennett <chrisdennett@gmail.com>
2019-04-17 16:01:30 +0100

    hid timer for now.

--- src/components/appBar/AnimationTimer.jsx
+++ src/components/appBar/AnimationTimer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { useSpring, animated } from "react-spring";
 
 const AnimationTimer = ({ toggleOn, toggleChange, animConfig }) => {
@@ -10,10 +11,23 @@ const AnimationTimer = ({ toggleOn, toggleChange, animConfig }) => {
   });
 
   return (
-    <div>
-      <animated.div style={props}>I will fade in</animated.div>
-    </div>
+    <div>{/* <animated.div style={props}>I will fade in</animated.div> */}</div>
   );
 };
 
 export default AnimationTimer;
+
+const darkColour = "#303030";
+const lightColour = "whitesmoke";
+
+const Outer = styled.div`
+  border-radius: 5px;
+  background: ${props => (props.isOn ? lightColour : darkColour)};
+  color: ${props => (props.isOn ? darkColour : lightColour)};
+  min-width: 48px;
+  padding: 8px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
